3c6689d4986c82204b7cc7ef4a1668651eb04d95
RMRAmadushanka <it19147260@my.sliit.lk>
2023-09-27 13:57:36 +0530

    updated pass

--- backend/passport.js
+++ backend/passport.js
@@ -7,7 +7,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
+			callbackURL: "https://itproject.onrender.com/auth/google/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
